f5a90a2881a05c6d1b8c578317d5b78b964491c4
Diego Sousa <pediegoss@gmail.com>
2024-05-11 08:02:30 -0300

    update

--- public/assets/js/env.js
+++ public/assets/js/env.js
@@ -1,19 +1,12 @@
 const configEnv = {
 	app_mode: 'development',
 	local_address: 'http://localhost:3007',
-	web_address: 'https://api.automatizavarejo.com.br',
+	web_address: 'https://api.automatizafacil.com.br',
 	geoLocator: 'https://api.ipgeolocation.io/ipgeo?apiKey=22d3753ff645418e9bf41d38dbf793e5',
 }
-
-// const configEnv = {
-// 	app_mode: 'development',
-// 	local_address: 'http://192.168.0.10:3007',
-// 	web_address: 'https://api.automatizavarejo.com.br',
-// 	geoLocator: 'https://api.ipgeolocation.io/ipgeo?apiKey=22d3753ff645418e9bf41d38dbf793e5'
-// }
 
 const npsConfigEnv = {
 	app_mode: configEnv.app_mode,
 	local_address: `${configEnv.local_address.split(':')[1]}:3008`,
-	web_address: 'https://pesquisa.automatizavarejo.com.br'
+	web_address: 'https://pesquisa.automatizafacil.com.br'
 }
